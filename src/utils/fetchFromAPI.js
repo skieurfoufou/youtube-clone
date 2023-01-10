0a8de91bd5c4558df737410a58bcd775fd20a07e
skieurfoufou <skieurfou@gmail.com>
2023-01-10 14:49:20 +0200

    bug fetch

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,13 +1,5 @@
 import axios from "axios";
-// const options = {
-//     method: 'GET',
-//     url: 'https://youtube-v31.p.rapidapi.com/captions',
-//     params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
-//     headers: {
-//       'X-RapidAPI-Key': 'cb16907690msh18f93810452a958p1296fbjsn88b4f9e030c3',
-//       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-//     }
-//   };
+
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
